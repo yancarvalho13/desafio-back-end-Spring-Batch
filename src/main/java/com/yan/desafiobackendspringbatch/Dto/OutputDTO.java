aed0a8cf6b98de7007d370b5961cee72b8869e29
yancarvalho13 <yansilva303@gmail.com>
2025-02-23 01:02:26 -0300

    ItemProcessor with witter patterns

--- src/main/java/com/yan/desafiobackendspringbatch/Dto/OutputDTO.java
+++ src/main/java/com/yan/desafiobackendspringbatch/Dto/OutputDTO.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.sql.Date;
 import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public record OutputDTO(Long id
 , Integer tipo
@@ -14,4 +16,24 @@ public record OutputDTO(Long id
 , Time hora
 , String donoDaLoja
 , String nomeDaLoja) {
+
+    /// Whiter patterns para tratar a diferença dos valores entre input e output
+
+    public OutputDTO withData(String data) throws ParseException {
+        var dateFormat = new SimpleDateFormat("yyyyMMdd");
+        var date = dateFormat.parse(data);
+
+        return new OutputDTO(this.id,this.tipo,new Date(date.getTime()),this.valor,this.cpf,this.cartao,this.hora,this.donoDaLoja,this.nomeDaLoja);
+    }
+
+    public OutputDTO withHora(String hora) throws ParseException {
+        var dateFormat = new SimpleDateFormat("HHmmss");
+        var hour = dateFormat.parse(hora);
+
+        return new OutputDTO(this.id,this.tipo,this.data,this.valor,this.cpf,this.cartao,new Time(hour.getTime()),this.donoDaLoja,this.nomeDaLoja);
+    }
+
+    public OutputDTO withValorDividedByHundred(BigDecimal valor) {
+        return new OutputDTO(this.id,this.tipo,this.data,valor.divide(BigDecimal.valueOf(100.00)),this.cpf,this.cartao,this.hora,this.donoDaLoja,this.nomeDaLoja);
+    }
 }
--- src/main/java/com/yan/desafiobackendspringbatch/infra/BatchConfig.java
+++ src/main/java/com/yan/desafiobackendspringbatch/infra/BatchConfig.java
@@ -12,7 +12,13 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.Range;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -25,6 +31,7 @@ public class BatchConfig {
         this.jobRepository = jobRepository;
     }
 
+    @Bean
     public Job job(Step step){
         return new JobBuilder("job", jobRepository)
                 .start(step)
@@ -33,6 +40,7 @@ public class BatchConfig {
 
     }
 
+    @Bean
     public Step step(ItemReader<InputDTO> reader, ItemProcessor<InputDTO,OutputDTO> processor, ItemWriter<OutputDTO> writer) {
         return new StepBuilder("step",jobRepository)
                 .<InputDTO, OutputDTO>chunk(1000,transactionManager)
@@ -40,5 +48,28 @@ public class BatchConfig {
                 .processor(processor)
                 .writer(writer)
                 .build();
+    }
+
+    @Bean
+    public FlatFileItemReader<InputDTO> reader(){
+        return new FlatFileItemReaderBuilder<InputDTO>()
+                .name("reader")
+                .resource(new FileSystemResource("/home/yanonjava/IdeaProjects/DesafioBackEnd-SpringBatch/CNAB.txt"))
+                .fixedLength()
+                .columns(new Range(1,1), new Range(2,9), new Range(10,19), new Range(20,30)
+                , new Range(31,42), new Range(43, 48), new Range(49,62), new Range(63,81))
+                .names("tipo","data","cpf","cartao","hora","donoDaLoja","nomeDaLoja")
+                .targetType(InputDTO.class)
+                .build();
+    }
+
+    public ItemProcessor<InputDTO,OutputDTO> processor(){
+        return item -> {
+            return new OutputDTO(null,
+                    item.tipo(), null,null,item.cpf(),item.cartao(),null,item.donoDaLoja().trim(),item.nomeDaLoja().trim())
+                    .withData(item.data())
+                    .withValorDividedByHundred(item.valor())
+                    .withHora(item.hora());
+        };
     }
 }
